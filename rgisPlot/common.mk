66d5ce7307b6a6999e7dd43474f6f9f7cc26739e
Administrator <admin@lily.ccny.cuny.edu>
2012-01-18 16:10:36 -0500

    Updating rgisPlot/common.mk to work with Ubuntu 11.10 Oneiric.

--- rgisPlot/common.mk
+++ rgisPlot/common.mk
@@ -1,21 +1,17 @@
 UNIX=$(shell uname)
 
-ifeq ($(UNIX),Linux)
-export UNIXLIBS=-lcpgplot -lpgplot -lpng -lm
-endif
-
 ifeq ($(UNIX),Darwin)
 ifndef ($(CUSTOM_INC)
 	CUSTOM_INC=-I/sw/include -I/sw/lib/pgplot
 endif
 ifndef ($(CUSTOM_LIB)
-	CUSTOM_LIB=-L/sw/lib -L/sw/lib/pgplot -L/sw/lib/gcc4.6/lib -L/usr/X11R6/lib
+	CUSTOM_LIB=-L/sw/lib -L/sw/lib/pgplot -L/sw/lib/gcc4.6/lib -L/usr/X11R6/lib e -Wl,-framework -Wl,Foundation -lgfortran -lz -laquaterm
 endif
-export UNIXLIBS=$(CUSTOM_LIB) -Wl,-framework -Wl,Foundation -lcpgplot -lpgplot -lgfortran -lz -laquaterm -lpng -lX11 -lm
 endif
 
 export UNIXCPP=g++
 export UNIXCPPOPS=-g -Wall -fsigned-char -D_GNU_SOURCE $(CUSTOM_INC)
+export UNIXLIBS=$(CUSTOM_LIB) -lcpgplot -lpgplot -lpng -lX11 -lm
 export UNIXMAKE=make
 
 ifndef INSTALLDIR
