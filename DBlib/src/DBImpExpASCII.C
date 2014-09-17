1b69c796a394389cf174738c831d04570339df27
Balazs Fekete <bfekete@ccny.cuny.edu>
2014-09-17 13:05:00 -0400

    Fixing record name in ASCII table import.

--- DBlib/src/DBImpExpASCII.C
+++ DBlib/src/DBImpExpASCII.C
@@ -234,7 +234,7 @@ DBInt DBImportASCIITable (DBObjTable *table, FILE *fp)
 	
 		strcpy (format,"Record%");
 		sprintf (format + 7,"0%dd",(int) (ceil (log10 (recordNum))));
-		printf ("%s\n",format);
+
 		for (recordID = 0;recordID < recordNum; recordID++)
 			{
 			sprintf (recordName,format,recordID + 1);
